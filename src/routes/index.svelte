97192c541fade08e2dce5d938e6d5545bc07dad2
Kirill Sofronov <2138870+sokirill@users.noreply.github.com>
2022-01-20 23:49:12 +0800

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,16 @@
+<script>
+	import TodoForm from '../components/TodoForm.svelte';
+  import {todos} from '../stores/todoStore'
+  import Todo from '../components/Todo.svelte'
+
+</script>
+
+<main>
+  <h1 class="text-2xl font-bold text-center text-gray-800 md:text-3xl">
+    My 2DOs
+  </h1>
+  <TodoForm />
+  {#each $todos as todo}
+    <Todo {todo} index={todo.id} />
+  {/each}
+</main>
